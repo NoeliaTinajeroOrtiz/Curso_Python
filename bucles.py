c0a0da45643f980a55ffffe02b2bf42a570f9165
Noelia Tinajero Ortiz <147175183+NoeliaTinajeroOrtiz@users.noreply.github.com>
2025-09-03 11:37:17 +0200

    Bucles

---
+++ bucles.py
@@ -0,0 +1,11 @@
+#Bloque FOR
+print("Números del 1 al 5 multiplicados por 2 con un bucle for: ")
+for numero in range( 1, 6 ):
+    print( numero * 2 )
+
+#Bloque WHILE
+print("\nNúmeros del 1 al 5 multiplicados por 2 con un bucle while:")
+contador = 1
+while contador <= 5:
+    print(contador * 2 )
+    contador += 1
